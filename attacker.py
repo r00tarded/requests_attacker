ce2c8bbca8de83991ce49dc9a6418eb89a656f3a
kazuki <kazu2055.tennis@gmail.com>
2018-02-05 14:48:20 +0900

    complete attack by requests module

--- attacker.py
+++ attacker.py
@@ -5,52 +5,50 @@ import requests
 
 def acccess(http_info):
     request_info = http_info
-    if "payload" not in http_info:
-        request_info["payload"] = {}
-    if "headers" not in http_info:
-        request_info["headers"] = {}
-    if "querystring" not in http_info:
-        request_info["querystring"] = {}
+    if 'payload' not in http_info:
+        request_info['payload'] = {}
+    if 'headers' not in http_info:
+        request_info['headers'] = {}
+    if 'querystring' not in http_info:
+        request_info['querystring'] = {}
 
     try:
         res = requests.request(
-            request_info["method"],
-            request_info["url"],
-            data=request_info["payload"],
-            headers=request_info["headers"],
-            params=request_info["querystring"],
+            request_info['method'],
+            request_info['url'],
+            data=request_info['payload'],
+            headers=request_info['headers'],
+            params=request_info['querystring'],
         )
         return res
     except Exception as e:
         print(e)
 
 
-def main():
+def main(user, pass_):
     res = acccess({
-        "method": "GET",
-        "url": "http://127.0.0.1:8000/admin",
+        'method': 'GET',
+        'url': 'http://127.0.0.1:8000/admin',
     })
-    print(res.cookies["csrftoken"])
+    cookie = res.cookies['csrftoken']
 
-# url = "http://127.0.0.1:8000/ja/admin/login/"
-#
-# querystring = {"next": "/ja/admin"}
-#
-# payload = "username=tom&password=tom&this_is_the_login_form=1&mezzanine_login_interface=admin&csrfmiddlewaretoken=DWOfqRpCpXkObnoYuutvWjBZMLKckpANC0m5exzmc9o6iFwn2gMrlKPPcjIh6BoX"
-# headers = {
-#     'cookie': "csrftoken=DWOfqRpCpXkObnoYuutvWjBZMLKckpANC0m5exzmc9o6iFwn2gMrlKPPcjIh6BoX",
-#     'referer': "http://127.0.0.1:8000/",
-#     'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-#     'content-type': "application/x-www-form-urlencoded",
-#     'origin': "http://127.0.0.1:8000",
-#     'cache-control': "no-cache",
-#     'postman-token': "65f3c7af-fb2a-553d-f697-bc31821e69ef"
-# }
-#
-# response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
-#
-# print(response.text)
+    res = acccess({
+        'method': 'POST',
+        'url': 'http://127.0.0.1:8000/ja/admin/login/',
+        'querystring': {'next': '/ja/admin'},
+        'payload': 'username={}&password={}&this_is_the_login_form=1&mezzanine_login_interface=admin&csrfmiddlewaretoken={}'.format(user, pass_, cookie),
+        'headers': {
+            'cookie': 'csrftoken={}'.format(cookie),
+            'referer': 'http://127.0.0.1:8000/',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'content-type': 'application/x-www-form-urlencoded',
+            'origin': 'http://127.0.0.1:8000',
+            'cache-control': 'no-cache',
+            'postman-token': '65f3c7af-fb2a-553d-f697-bc31821e69ef'
+        },
+    })
+    print(res.text)
 
 
 if __name__ == '__main__':
-    main()
+    main("tom", "tom")
